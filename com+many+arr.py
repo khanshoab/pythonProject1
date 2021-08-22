43a6733b87c3ab66be0e2ce9a95f1c5316c0cc75
khan shoab <khanshoab19co33@gmail.com>
2021-08-22 20:18:26 +0530

    This is all about how compare many array using numpy in python.

---
+++ com+many+arr.py
@@ -0,0 +1,10 @@
+print("how to compare value in array using comparison operators")
+from numpy import *
+a = array([10,60,30,40])
+b = array([50,60,70,40])
+value = a == b   # equal to b
+print(value)
+d = a < b  # greater than a
+print(d)
+v = a > b  # less than  a
+print(v)
