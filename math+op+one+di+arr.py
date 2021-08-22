e628e08050166cb9f7756d10202dfab3dc551514
khan shoab <khanshoab19co33@gmail.com>
2021-08-22 19:55:40 +0530

    This is the zeros function in one dimensional array

---
+++ math+op+one+di+arr.py
@@ -0,0 +1,17 @@
+print("mathematical operation on array using numpy")
+from numpy import *
+a = array([11,22,14,15,17])
+b = array([21,22,23,24,25])
+print("two array addition")
+c = a+b
+print(c)
+print("addition")
+a = a+5
+print(a)
+print("subtractions")
+b = b-5
+print(a)
+print("using for loop")
+for el in c:
+    print(el)
+
