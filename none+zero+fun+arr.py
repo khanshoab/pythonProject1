5dd852a9dceea9e73de355d9005c6e12ba474f44
khan shoab <khanshoab19co33@gmail.com>
2021-08-24 17:17:36 +0530

    This is used to determine non zero element, it gives new array.

---
+++ none+zero+fun+arr.py
@@ -0,0 +1,8 @@
+# none zero function- This function is used to determine the position of elements which are non zero .
+from numpy import *
+a = array([12,13,14,0,38,0])
+result = nonzero(a)
+print(result)
+
+
+# This function returns an array that contains the index of the element of the array which are not equal to zero.
