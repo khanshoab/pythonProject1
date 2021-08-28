e4e4a2c8cfe1b088f630dc318c47b841568551fe
khan shoab <khanshoab19co33@gmail.com>
2021-08-28 20:31:04 +0530

    Create 2D array using array () function.

---
+++ mul+di+arr.py
@@ -0,0 +1,10 @@
+# Multi-dimensional Array means 2d ,3d ,4d etc.
+# It is also known as array of arrays.
+# create 2d array using array () function.
+from numpy import *
+a = array([[23,33,32,43],
+           [54,23,35,23]])
+print(a,dtype)
+print(a[1][3])
+a[0][1] = 100
+print(a[0][1])
