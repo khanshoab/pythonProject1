f21599bef9dbc350333deae8ec6002aaf1c91518
khan shoab <khanshoab19co33@gmail.com>
2021-09-03 20:45:36 +0530

    This is all about Concatenation operator in python.

---
+++ concatenation+ope.py
@@ -0,0 +1,12 @@
+# Concatenation operator is used to join the two string.
+print("khan"+"bro")
+print()
+str1 = "khan"
+str2 = "bhaii"
+str3 = str1 + str2
+print(str3)
+print()
+str4 = "are you"
+print("Hello How "+str4)
+print("hello how",str4)
+print("hello"+str4+"happay")
