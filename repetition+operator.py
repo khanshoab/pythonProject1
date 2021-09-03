6ecdaa406ce88a855e788339587f33bb80820864
khan shoab <khanshoab19co33@gmail.com>
2021-09-03 20:35:31 +0530

    Repetition operator.

---
+++ repetition+operator.py
@@ -0,0 +1,8 @@
+# Repetition operator is used to repeat the string for several times.
+# It is denoted by *
+print("$" * 10)
+str1 = "MyMother "
+print(str1 * 10)
+# slicing string
+str2 = "my dream "
+print(str2[0:2] * 5)   # To printing the 'my
