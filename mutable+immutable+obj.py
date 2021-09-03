5294075d012a076b1699c73d55c97cb526883f41
khan shoab <khanshoab19co33@gmail.com>
2021-09-03 20:03:43 +0530

    Mutable and Immutable object in python.

---
+++ mutable+immutable+obj.py
@@ -0,0 +1,6 @@
+# Mutable and Immutable object in python.
+# Mutable object : Mutable objects are those object whose value or content can be changed as and when required.
+# ex: list , set, dictionaries
+
+# Immutable object : Immutable objects are those object whose value or content can not be changed.
+# ex: number, string , tuple
