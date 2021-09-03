716fa1d49b4c8593cf4fbc17bb16eed7b60026a1
khan shoab <khanshoab19co33@gmail.com>
2021-09-03 19:54:18 +0530

    Accessing string length using loop.

---
+++ access+using+loop.py
@@ -0,0 +1,20 @@
+str1 = "indian"
+for i in str1:
+    print(i)
+print()
+# with index
+str2 = "language"
+o = len(str2)
+print(o)
+for c in range(o):
+    print(str2[c])
+print()
+
+# using while loop
+str3 = "formula"
+p = len(str3)
+k = 0
+while k < p:
+    print(str3[k])
+    k += 1
+
