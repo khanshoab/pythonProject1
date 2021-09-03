7bf36a9c07a1fc01fe1d40ad0adcac7040b8583b
khan shoab <khanshoab19co33@gmail.com>
2021-09-03 19:31:29 +0530

    This is all about the string length.

---
+++ string+length.py
@@ -0,0 +1,10 @@
+# Length of string is the no. of character in a string.
+# len() function is used to grt the length of the string.
+str1 = "independence"
+n = len(str1)
+print(n)
+print()
+
+str2 = 'language'
+m = len(str2)
+print(m)
