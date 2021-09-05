fd00713d7031405cbbac917acf90c542a1952fe5
khan shoab <khanshoab19co33@gmail.com>
2021-09-05 20:26:46 +0530

    This is all about Comparing string in python.

---
+++ comparing+str.py
@@ -0,0 +1,37 @@
+# Comparing string in python
+str1 = "INDIAN"
+str2 = "INDIAN"
+result = str1 == str2
+print(result)
+
+str1 = "JAVA"
+str2 = "PYTHON"
+result = str1 == str2
+print(result)
+
+str1 = "a"
+str2 = "b"
+result = str1 > str2     # indexing is start with a=0 ,b=1 ,c=2 like.
+print(result)          # a(0) > b(1)   = false
+
+str1 = "a"
+str2 = "b"
+result = str1 < str2    # which alphabet is coming first in A to Z
+print(result)           #
+
+str1 = "a"
+str2 = "b"
+result = str1 > str2    # which alphabet is coming first in A to Z
+print(result)           # (0) >
+
+str1 = "PYTHON"
+str2 = "JAVA"
+result = str1 > str2    # which alphabet is coming first in A to Z
+print(result)
+
+str1 = "acorn"
+str2 = "acoustic"
+result = str1 < str2    # which alphabet is coming first in A to Z
+print(result)
+
+
