3b44ed6a8149b1e7ed78fdb8616b8b74cefa04b1
khan shoab <khanshoab19co33@gmail.com>
2021-09-15 20:08:49 +0530

    This is all about the string function.

---
+++ string+function.py
@@ -0,0 +1,28 @@
+# upper () This method is used to convert all character of a string into uppercase
+name = "khan bro"
+print(name)
+print(name.upper())
+str1 = name.upper()
+print(str1)
+
+# lower () This method is used to convert all character of a string into lowercase
+name = "khan bro what to do"
+print(name)
+print(name.lower())
+str1 = name.lower()
+print(str1)
+
+# swap case () This method is used to convert all lowercase character into uppercase and vice - versa
+name = "khan bro where are you going"
+print(name)
+print(name.swapcase())
+str1 = name.swapcase()
+print(str1)
+
+# title () This method is used to convert the string in such that each word in string will start with a capital letter
+# and remaining will be small letter.
+name = "hello everyone my name is khan"
+print(name)
+print(name.title())
+str1 = name.title()
+print(str1)
