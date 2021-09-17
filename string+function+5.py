c639fce2be8e9e96a0793fd519bf4613d92b65cc
khan shoab <khanshoab19co33@gmail.com>
2021-09-17 21:25:18 +0530

    replace split and join string function.

---
+++ string+function+5.py
@@ -0,0 +1,17 @@
+# replace() -This method is used to replace a sub string in a string with another sub string.
+name = "HelloMumbai"
+old = "Mumbai"
+new = "Dubai"
+str1 = name.replace(old, new)
+print(name)
+print(str1)
+# split() -This method is used to split/break a string into pieces.These pieces returns as a list.
+name = "'Hello','how','are','you'"
+str2 = name.split('-')
+print(name)
+print(str2)
+# join() -This method is used to join string into one string.
+name = ('Hello',' how',' are',' you')
+str3 = ''.join(name)
+print(name)
+print(str3)
