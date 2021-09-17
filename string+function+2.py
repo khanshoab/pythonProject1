1e071e34afd3a174c5c99699d992b19240985b87
khan shoab <khanshoab19co33@gmail.com>
2021-09-17 20:21:38 +0530

    This is all about the string function isdigit, isalpha, isalnum.

---
+++ string+function+2.py
@@ -0,0 +1,38 @@
+# isdigit() -This method returns True if the string contain only numeric digits ( 0 to 9) else returns False.
+print("isdigit")
+name = "4628796427"
+print(name.isdigit())
+num = "6492hj82748"
+print(num.isdigit())
+
+hello = "649283789"
+str1 = hello.isdigit()
+print(hello)
+print(str1)
+
+# isalpha() -This method returns True if the string at least one character and all are alphabet (A to Z and a to z),
+# else returns False.
+print("isalpha")
+name = "hello friend"
+print(name.isalpha())
+num = "6492hj827G4L8"
+print(num.isalpha())
+
+hello = "My name is shabby"
+str1 = hello.isalpha()
+print(hello)
+print(str1)
+
+
+# isdigit() -This method returns True if the string has at least one character and all character in the string are,
+# alphanumeric (A to Z ,a to z and 0 to 9) else returns False.
+print("isalnum")
+name = "4628796427"
+print(name.isalnum())
+num = "6492hj82748"
+print(num.isalnum())
+
+hello = "64928jh3hg7k89"
+str1 = hello.isalnum()
+print(hello)
+print(str1)
