75680821caaf15c01d894122c1c203672385aae5
khan shoab <khanshoab19co33@gmail.com>
2021-09-17 20:28:08 +0530

    This is about the string function isspace.

---
+++ string+function+3.py
@@ -0,0 +1,5 @@
+# isspace() - This method returns True if the string contains only space else returns False.
+name = "     "
+print(name.isspace())
+num = "    hello everyone           how are you"
+print(num.isspace())
