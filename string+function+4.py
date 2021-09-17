acb06a64df4246fc09e9e02f257086a60b265b99
khan shoab <khanshoab19co33@gmail.com>
2021-09-17 20:53:31 +0530

    This is about the spacing between the string function lstrip, rstrip, strip.

---
+++ string+function+4.py
@@ -0,0 +1,15 @@
+# lstrip() - This method is used to remove the space which are at left side of the string.
+num = "     everyone"
+str1 = num.lstrip()
+print(num)
+print(str1)
+# rstrip() - This method is used to remove the space which are at right side of the string.
+num = "everyone    "
+str1 = num.rstrip()
+print(num)
+print(str1)
+# strip() - This method is used to remove the space from the both side of the strong.
+num = "     everyone     "
+str1 = num.strip()
+print(num)
+print(str1)
