b9241e8c1dd8c12e309d6fd960d0fc2544ab5c2a
khan shoab <khanshoab19co33@gmail.com>
2021-09-28 08:34:56 +0530

    1. Positional Arguments.

---
+++ types+of+actual+ar.py
@@ -0,0 +1,16 @@
+# Types of Actual Arguments
+# 1.Position arguments
+# These arguments are passed to the function in correct positional order.
+# The number of arguments and their position in the function definition should be equal to the number
+# and position of the arguments in the function call.
+def go(a,b):
+    z = a**b
+    print(z)
+
+go(5,2)
+
+# 2.keyword arguments
+# 3.default arguments
+# 4.variable length arguments
+# 5.keyword variable length arguments
+
